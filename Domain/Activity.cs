5b1ed0d3a71b04173afcdb4bbf5bc964239e49ab
Badr <bsh20351badersh20002@gmail.com>
2022-11-04 22:37:01 +0200

    creating our skelton api

---
+++ Domain/Activity.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Activity
+    {
+
+        //It must be Id with capital
+        //These are our columns
+        public Guid Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Category { get; set; }
+
+        public string? City { get; set; }
+
+        public string? Venue { get; set; }
+
+
+
+
+    }
+}
